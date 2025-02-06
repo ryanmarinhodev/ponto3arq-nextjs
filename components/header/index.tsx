a428369fe05f7c3999e72510c1b82783d80b4093
ryanmarinhodev <ryanmarinhodev@gmail.com>
2025-02-06 18:12:47 -0300

    Adicionando menu hamburguer

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,25 +1,53 @@
+"use client";
 import Logo from "../../public/logo.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import { FaInstagram } from "react-icons/fa";
 import "./styles.css";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdClose } from "react-icons/io";
 
 export function Header() {
+  const pathname = usePathname();
+
+  const [navItem, setNavItem] = useState(false);
+
+  const clickMenu = () => {
+    setNavItem(!navItem);
+  };
+
   return (
     <header className="header">
       <nav className="navbar">
-        <Link href="/" className="logo-container">
-          <Image
-            src={Logo}
-            alt="Logo"
-            width={200}
-            height={50}
-            className="logo"
-          />
-        </Link>
-        <Link href={"/"} target="_blank" className="instagram-icon">
-          <FaInstagram size={20} />
-        </Link>
+        <div className="hamburguer">
+          {navItem ? (
+            <IoMdClose onClick={clickMenu} />
+          ) : (
+            <GiHamburgerMenu onClick={clickMenu} />
+          )}
+        </div>
+        <div
+          className={`left-links ${navItem ? "open" : "close"} ${
+            pathname === "/" ? "hidden-links" : ""
+          }`}
+        >
+          <Link href="/">Escritório</Link>
+          <Link href="/contatos">Contato</Link>
+        </div>
+
+        <div className="logo-container">
+          <Link href="/">
+            <Image src={Logo} alt="Logo" className="logo" />
+          </Link>
+        </div>
+
+        <div className="right-links">
+          <Link href="/" target="_blank" className="instagram-icon">
+            <FaInstagram size={20} />
+          </Link>
+        </div>
       </nav>
     </header>
   );
