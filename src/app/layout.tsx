1dd458854c94e2f403bd05201c4700c14ef6e97c
ryanmarinhodev <ryanmarinhodev@gmail.com>
2025-03-21 11:40:08 -0300

    Adicionando spinner a home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import { Header } from "../../components/header/index";
 import { Poppins } from "next/font/google";
+import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { Spinner } from "../../components/spinner/spinner";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -12,9 +17,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const [loading, setLoading] = useState(true);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => setLoading(false), 800);
+    return () => clearTimeout(timer);
+  }, [pathname]);
+
   return (
     <html lang="pt-br">
       <body suppressHydrationWarning={true} className={poppins.className}>
+        {loading ? <Spinner /> : null}
         <Header />
         {children}
       </body>
