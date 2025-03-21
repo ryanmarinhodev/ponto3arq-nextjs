374f5489dbe79c5b2aa0ffb0ee368eae40159782
ryanmarinhodev <ryanmarinhodev@gmail.com>
2025-03-21 11:36:47 -0300

    Adicionando spinner useEffect a pagina

---
+++ components/spinner/spinner.tsx
@@ -0,0 +1,9 @@
+import styles from "./spinner.module.css";
+
+export const Spinner = () => {
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.loader}></div>
+    </div>
+  );
+};
