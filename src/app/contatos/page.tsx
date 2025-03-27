9f9413a16263f243416b0c480efe24578d8cdb35
ryanmarinhodev <ryanmarinhodev@gmail.com>
2025-03-27 12:47:33 -0300

    Estilizando o contatos

--- src/app/contatos/page.tsx
+++ src/app/contatos/page.tsx
@@ -1,7 +1,33 @@
+import "./styles.css";
+import Link from "next/link";
+
 export default function Contato() {
+  const whatsappMessage =
+    "Olá, vim pelo site e gostaria de saber mais informações sobre os serviços!";
+  const whatsappNumber = "5583991321818";
+  const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
+    whatsappMessage
+  )}`;
+
   return (
-    <div>
-      <h1>Contato</h1>
+    <div className="container">
+      <h1>João Pessoa</h1>
+      <p>Av. Silvino Chaves, 360 - Manaíra, João Pessoa - PB, 58038-420</p>
+      <p>
+        Telefone:
+        <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
+          +55 (83) 99132-1818
+        </a>
+      </p>
+      <p>
+        Email:
+        <Link
+          legacyBehavior
+          href="mailto:contato@ponto3arq.com.br?subject=Contato pelo site&body=Olá, vim pelo site e gostaria de obter mais informações."
+        >
+          <a>contato@ponto3arq.com.br</a>
+        </Link>
+      </p>
     </div>
   );
 }
